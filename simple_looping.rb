49a8ccdc306fb6325fd933075d96c69bf26f49d1
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-19 18:55:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -45,6 +45,7 @@ phrase = "Welcome to Flatiron School's Web Development Course!"
   counter += 1 
   puts phrase
 end
+end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
